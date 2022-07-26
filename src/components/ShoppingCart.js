9109df60610a7bd5eebcc428356774badb4a5fcb
JulioHudson <100172036+JulioHudson@users.noreply.github.com>
2022-07-26 18:36:23 -0300

    Desafío: Item Collection II

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { addDoc, collection, getFirestore, startAfter } from "firebase/firestore";
+import Swal from 'sweetalert2'
+import { link } from 'react-router-dom'
 import { useCartContext } from "../context/CartContext";
 import ItemInCart from "./ItemInCart";
 
 const ShoppingCart = () => {
     const { cart, totalPrice, clearCart } = useCartContext();
 
-    if (cart.length === 0) {
+    const order = {
+        buyer: {
+            name: 'John',
+            email: 'john@example.com',
+            phone: '123-456-1234',
+            address: 'asdasdasdasdasdasdasdasdasdas'
+        },
+        items: cart.map(product => ({id: product.id, name: product.name, price: product.price, quantity: product.quantity})),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+        .then(({ id }) => console.log(id))
+    }
+
+    if (cart.length === 0) 
         return (
             <>
             <div className='block'>
@@ -13,7 +34,7 @@ const ShoppingCart = () => {
             </div>
             </>
         );
-}
+
     return (
         <>
             <div className='block'>
@@ -24,6 +45,7 @@ const ShoppingCart = () => {
                     Total: {totalPrice()}
                 </p>
                 <button onClick={clearCart}>Vaciar carrito</button>
+                <button onClick={handleClick}>Confirmar compra</button>
             </div>
         </>
     )
